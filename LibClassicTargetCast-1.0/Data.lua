ccdb1d71eec3f232174300a0618640abce75e1b6
Joe Vaughan <joe.vaughan@gmail.com>
2019-06-10 16:27:50 +0100

    Initial commit of working code - check my AzCastbar_classic repository for a working example of this library in use

---
+++ LibClassicTargetCast-1.0/Data.lua
@@ -0,0 +1,172 @@
+-- Taken from ClassicCastBars - All credit to wardz for the excellent work and the technique of getting target cast info from the CLEU
+local _, ns = ...
+local GetSpellInfo = _G.GetSpellInfo
+
+-- Channeled spells does not return cast time, so we have to build our own list.
+--
+-- We use GetSpellInfo here to get the localized spell name,
+-- that way we don't have to list every spellID for an ability (diff ranks have diff id)
+ns.channeledSpells = {
+    -- MISC
+    [GetSpellInfo(746)] = 7,        -- First Aid
+    [GetSpellInfo(13278)] = 4,      -- Gnomish Death Ray
+    [GetSpellInfo(20577)] = 10,     -- Cannibalize
+
+    -- DRUID
+    [GetSpellInfo(17401)] = 9.5,    -- Hurricane
+    [GetSpellInfo(740)] = 9.5,      -- Tranquility
+
+    -- HUNTER
+    [GetSpellInfo(6197)] = 60,      -- Eagle Eye
+    [GetSpellInfo(1002)] = 60,      -- Eyes of the Beast
+    [GetSpellInfo(20900)] = 3,      -- Aimed Shot TODO: verify
+
+    -- MAGE
+    [GetSpellInfo(5143)] = 4.5,     -- Arcane Missiles
+    [GetSpellInfo(10)] = 7.5,       -- Blizzard
+    [GetSpellInfo(12051)] = 8,      -- Evocation
+
+    -- PRIEST
+    [GetSpellInfo(15407)] = 3,      -- Mind Flay
+    [GetSpellInfo(2096)] = 60,      -- Mind Vision
+    [GetSpellInfo(605)] = 3,        -- Mind Control
+
+    -- WARLOCK
+    [GetSpellInfo(689)] = 4.5,      -- Drain Life
+    [GetSpellInfo(5138)] = 4.5,     -- Drain Mana
+    [GetSpellInfo(1120)] = 14.5,    -- Drain Soul
+    [GetSpellInfo(5740)] = 7.5,     -- Rain of Fire
+    [GetSpellInfo(1949)] = 15,      -- Hellfire
+    [GetSpellInfo(755)] = 10,       -- Health Funnel
+}
+
+-- List of abilities that makes cast time slower.
+-- Spells here have different % reduction based on spell rank,
+-- so list by spellID instead of name here so we can diff between ranks
+ns.castTimeDecreases = {
+    -- WARLOCK
+    [1714] = 50,    -- Curse of Tongues Rank 1
+    [11719] = 60,   -- Curse of Tongues Rank 2
+
+    -- ROGUE
+    [5760] = 40,    -- Mind-Numbing Poison Rank 1
+    [8692] = 50,    -- Mind-Numbing Poison Rank 2
+    [25810] = 50,   -- Mind-Numbing Poison Rank 2 incorrect?
+    [11398] = 60,   -- Mind-Numbing Poison Rank 3
+
+    -- ITEMS
+    [17331] = 10,   -- Fang of the Crystal Spider
+}
+
+-- Spells that often have cast time reduced by talents
+ns.castTimeTalentDecreases = {
+    [GetSpellInfo(403)] = 1,        -- Lightning Bolt
+    [GetSpellInfo(421)] = 1,        -- Chain Lightning
+    [GetSpellInfo(6353)] = 2,       -- Soul Fire
+    [GetSpellInfo(116)] = 0.5,      -- Frostbolt
+    [GetSpellInfo(133)] = 0.5,      -- Fireball
+    [GetSpellInfo(686)] = 0.5,      -- Shadow Bolt
+    [GetSpellInfo(348)] = 0.5,      -- Immolate
+    [GetSpellInfo(331)] = 0.5,      -- Healing Wave
+    [GetSpellInfo(585)] = 0.5,      -- Smite
+    [GetSpellInfo(14914)] = 0.5,    -- Holy Fire
+    [GetSpellInfo(2054)] = 0.5,     -- Heal
+    [GetSpellInfo(25314)] = 0.5,    -- Greater Heal
+    [GetSpellInfo(8129)] = 0.5,     -- Mana Burn
+    [GetSpellInfo(5176)] = 0.5,     -- Wrath
+    [GetSpellInfo(2912)] = 0.5,     -- Starfire
+    [GetSpellInfo(5185)] = 0.5,     -- Healing Touch
+    [GetSpellInfo(2645)] = 2,       -- Ghost Wolf
+    [GetSpellInfo(691)] = 4,        -- Summon Felhunter
+    [GetSpellInfo(688)] = 4,        -- Summon Imp
+    [GetSpellInfo(697)] = 4,        -- Summon Voidwalker
+    [GetSpellInfo(712)] = 4,        -- Summon Succubus
+}
+
+-- List of player crowd controls
+-- We want to stop the castbar when these auras are detected
+-- as SPELL_CAST_FAILED is not triggered when a player gets CC'ed.
+ns.crowdControls = {
+    [GetSpellInfo(5211)] = 1,       -- Bash
+    [GetSpellInfo(24394)] = 1,      -- Intimidation
+    [GetSpellInfo(853)] = 1,        -- Hammer of Justice
+    [GetSpellInfo(22703)] = 1,      -- Inferno Effect (Summon Infernal)
+    [GetSpellInfo(408)] = 1,        -- Kidney Shot
+    [GetSpellInfo(12809)] = 1,      -- Concussion Blow
+    [GetSpellInfo(20253)] = 1,      -- Intercept Stun
+    [GetSpellInfo(20549)] = 1,      -- War Stomp
+    [GetSpellInfo(2637)] = 1,       -- Hibernate
+    [GetSpellInfo(3355)] = 1,       -- Freezing Trap
+    [GetSpellInfo(19386)] = 1,      -- Wyvern Sting
+    [GetSpellInfo(118)] = 1,        -- Polymorph
+    [GetSpellInfo(28271)] = 1,      -- Polymorph: Turtle
+    [GetSpellInfo(28272)] = 1,      -- Polymorph: Pig
+    [GetSpellInfo(20066)] = 1,      -- Repentance
+    [GetSpellInfo(1776)] = 1,       -- Gouge
+    [GetSpellInfo(6770)] = 1,       -- Sap
+    [GetSpellInfo(1513)] = 1,       -- Scare Beast
+    [GetSpellInfo(8122)] = 1,       -- Psychic Scream
+    [GetSpellInfo(2094)] = 1,       -- Blind
+    [GetSpellInfo(5782)] = 1,       -- Fear
+    [GetSpellInfo(5484)] = 1,       -- Howl of Terror
+    [GetSpellInfo(6358)] = 1,       -- Seduction
+    [GetSpellInfo(5246)] = 1,       -- Intimidating Shout
+    [GetSpellInfo(6789)] = 1,       -- Death Coil
+    [GetSpellInfo(9005)] = 1,       -- Pounce
+    [GetSpellInfo(1833)] = 1,       -- Cheap Shot
+    [GetSpellInfo(16922)] = 1,      -- Improved Starfire
+    [GetSpellInfo(19410)] = 1,      -- Improved Concussive Shot
+    [GetSpellInfo(12355)] = 1,      -- Impact
+    [GetSpellInfo(20170)] = 1,      -- Seal of Justice Stun
+    [GetSpellInfo(15269)] = 1,      -- Blackout
+    [GetSpellInfo(18093)] = 1,      -- Pyroclasm
+    [GetSpellInfo(12798)] = 1,      -- Revenge Stun
+    [GetSpellInfo(5530)] = 1,       -- Mace Stun
+    [GetSpellInfo(19503)] = 1,      -- Scatter Shot
+    [GetSpellInfo(605)] = 1,        -- Mind Control
+    [GetSpellInfo(7922)] = 1,       -- Charge Stun
+    [GetSpellInfo(18469)] = 1,      -- Counterspell - Silenced
+    [GetSpellInfo(15487)] = 1,      -- Silence
+    [GetSpellInfo(18425)] = 1,      -- Kick - Silenced
+    [GetSpellInfo(24259)] = 1,      -- Spell Lock
+    [GetSpellInfo(18498)] = 1,      -- Shield Bash - Silenced
+
+    -- ITEMS
+    [GetSpellInfo(13327)] = 1,      -- Reckless Charge
+    [GetSpellInfo(1090)] = 1,       -- Sleep
+    [GetSpellInfo(5134)] = 1,       -- Flash Bomb Fear
+    [GetSpellInfo(19821)] = 1,      -- Arcane Bomb Silence
+    [GetSpellInfo(4068)] = 1,       -- Iron Grenade
+    [GetSpellInfo(19769)] = 1,      -- Thorium Grenade
+    [GetSpellInfo(13808)] = 1,      -- M73 Frag Grenade
+    [GetSpellInfo(4069)] = 1,       -- Big Iron Bomb
+    [GetSpellInfo(12543)] = 1,      -- Hi-Explosive Bomb
+    [GetSpellInfo(4064)] = 1,       -- Rough Copper Bomb
+    [GetSpellInfo(12421)] = 1,      -- Mithril Frag Bomb
+    [GetSpellInfo(19784)] = 1,      -- Dark Iron Bomb
+    [GetSpellInfo(4067)] = 1,       -- Big Bronze Bomb
+    [GetSpellInfo(4066)] = 1,       -- Small Bronze Bomb
+    [GetSpellInfo(4065)] = 1,       -- Large Copper Bomb
+    [GetSpellInfo(13237)] = 1,      -- Goblin Mortar
+    [GetSpellInfo(835)] = 1,        -- Tidal Charm
+    [GetSpellInfo(13181)] = 1,      -- Gnomish Mind Control Cap
+    [GetSpellInfo(12562)] = 1,      -- The Big One
+    [GetSpellInfo(15283)] = 1,      -- Stunning Blow (Weapon Proc)
+    [GetSpellInfo(56)] = 1,         -- Stun (Weapon Proc)
+    [GetSpellInfo(26108)] = 1,      -- Glimpse of Madness
+}
+
+--[[
+namespace.castTimeIncreases = {
+    -- HUNTER
+    [GetSpellInfo(3045)] = 45,    -- Rapid Fire
+
+    -- MAGE
+    [GetSpellInfo(23723)] = 33,   -- Mind Quickening
+}
+
+namespace.pushbackImmunities = {
+    -- PRIEST
+    [GetSpellInfo(14743)] = 1, -- Focused Casting
+}]]
+
---
+++ LibClassicTargetCast-1.0/LibClassicTargetCast-1.0.lua
@@ -0,0 +1,278 @@
+local _, ns = ...
+
+local MAJOR, MINOR = "LibClassicTargetCast-1.0", 1
+local LibStub = LibStub
+local lib = LibStub:NewLibrary(MAJOR, MINOR)
+if not lib then return end
+
+local _G = getfenv(0)
+
+local tinsert = _G.tinsert
+local unpack = _G.unpack
+local pairs = _G.pairs
+local tremove = _G.tremove
+local tsort = _G.table.sort
+local tContains = _G.tContains
+local ipairs = _G.ipairs
+local wipe = _G.wipe
+
+local channeledSpells = ns.channeledSpells
+local castTimeDecreases = ns.castTimeDecreases
+local castTimeTalentDecreases = ns.castTimeTalentDecreases
+local crowdControls = ns.crowdControls
+
+lib.callbacks = lib.callbacks or LibStub("CallbackHandler-1.0"):New(lib)
+
+lib.spellCache = {};
+
+logScanner=CreateFrame("Frame");
+
+logScanner:RegisterEvent("PLAYER_LOGIN")
+
+logScanner:SetScript("OnEvent", function(self, event, ...)
+    -- this will basically trigger EVENT_NAME(arguments)
+    return self[event](self, ...)
+end)
+
+function logScanner:CastPushback(unitGUID, percentageAmount, auraFaded)
+    local cast = lib.spellCache[unitGUID]
+    if not cast then return end
+
+    -- if cast.prevCurrTimeModValue then print("stored total:", #cast.prevCurrTimeModValue) end
+
+    -- Set cast time modifier (i.e Curse of Tongues)
+    if not auraFaded and percentageAmount and percentageAmount > 0 then
+        if not cast.currTimeModValue or cast.currTimeModValue < percentageAmount then -- run only once unless % changed to higher val
+            if cast.currTimeModValue then -- already was reduced
+                -- if existing modifer is e.g 50% and new is 60%, we only want to adjust cast by 10%
+                percentageAmount = percentageAmount - cast.currTimeModValue
+
+                -- Store previous lesser modifier that was active
+                cast.prevCurrTimeModValue = cast.prevCurrTimeModValue or {}
+                cast.prevCurrTimeModValue[#cast.prevCurrTimeModValue + 1] = cast.currTimeModValue
+                -- print("stored lesser modifier")
+            end
+
+            -- print("refreshing timer", percentageAmount)
+            cast.currTimeModValue = (cast.currTimeModValue or 0) + percentageAmount -- highest active modifier
+            cast.maxValue = cast.maxValue + (cast.maxValue * percentageAmount) / 100
+            cast.endTime = cast.endTime + (cast.maxValue * percentageAmount) / 100
+        elseif cast.currTimeModValue == percentageAmount then
+            -- new modifier has same percentage as current active one, just store it for later
+            -- print("same percentage, storing")
+            cast.prevCurrTimeModValue = cast.prevCurrTimeModValue or {}
+            cast.prevCurrTimeModValue[#cast.prevCurrTimeModValue + 1] = percentageAmount
+        end
+    elseif auraFaded and percentageAmount then
+        -- Reset cast time modifier
+        if cast.currTimeModValue == percentageAmount then
+            cast.maxValue = cast.maxValue - (cast.maxValue * percentageAmount) / 100
+            cast.endTime = cast.endTime - (cast.maxValue * percentageAmount) / 100
+            cast.currTimeModValue = nil
+
+            -- Reset to lesser modifier if available
+            if cast.prevCurrTimeModValue then
+                local highest, index = 0
+                for i = 1, #cast.prevCurrTimeModValue do
+                    if cast.prevCurrTimeModValue[i] and cast.prevCurrTimeModValue[i] > highest then
+                        highest, index = cast.prevCurrTimeModValue[i], i
+                    end
+                end
+
+                if index then
+                    cast.prevCurrTimeModValue[index] = nil
+                    -- print("resetting to lesser modifier", highest)
+                    return self:CastPushback(unitGUID, highest)
+                end
+            end
+        end
+
+        if cast.prevCurrTimeModValue then
+            -- Delete 1 old modifier (doesn't matter which one aslong as its the same %)
+            for i = 1, #cast.prevCurrTimeModValue do
+                if cast.prevCurrTimeModValue[i] == percentageAmount then
+                    -- print("deleted lesser modifier, new total:", #cast.prevCurrTimeModValue - 1)
+                    cast.prevCurrTimeModValue[i] = nil
+                    return
+                end
+            end
+        end
+    else -- normal pushback
+        if not cast.isChanneled then
+            cast.maxValue = cast.maxValue + 0.5
+            cast.endTime = cast.endTime + 0.5
+        else
+            -- channels are reduced by 25%
+            cast.maxValue = cast.maxValue - (cast.maxValue * 25) / 100
+            cast.endTime = cast.endTime - (cast.maxValue * 25) / 100
+        end
+    end
+end
+
+function lib:UnitCastingInfo(unit)
+    local unitGUID = UnitGUID(unit)
+    if lib.spellCache[unitGUID] and not lib.spellCache[unitGUID].isChanneled then 
+        return lib.spellCache[unitGUID].spellName, lib.spellCache[unitGUID].rank, lib.spellCache[unitGUID].spellIcon, lib.spellCache[unitGUID].startTime, lib.spellCache[unitGUID].endTime, false, lib.spellCache[unitGUID].castID,false
+    end
+end
+
+function lib:UnitChannelInfo(unit)
+    local unitGUID = UnitGUID(unit)
+    if lib.spellCache[unitGUID] and lib.spellCache[unitGUID].isChanneled then 
+        return lib.spellCache[unitGUID].spellName, lib.spellCache[unitGUID].rank, lib.spellCache[unitGUID].spellIcon, lib.spellCache[unitGUID].startTime, lib.spellCache[unitGUID].endTime, false, false
+    end
+end
+
+function logScanner:PLAYER_LOGIN()
+    self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+    self:RegisterEvent("PLAYER_ENTERING_WORLD")
+    self:RegisterEvent("PLAYER_TARGET_CHANGED")
+    self:UnregisterEvent("PLAYER_LOGIN")
+end
+
+function logScanner:PLAYER_ENTERING_WORLD()
+    -- Clear the Cache
+    lib.spellCache = {};
+    -- Fire in case consuming addon needs to initialise anything
+    lib.callbacks:Fire("PLAYER_ENTERING_WORLD")
+end
+
+function logScanner:PLAYER_TARGET_CHANGED()
+    local target = UnitGUID("target")
+    if lib.spellCache[target] then
+        lib.callbacks:Fire("PLAYER_TARGET_CHANGED","target",lib.spellCache[target].castID,lib.spellCache[target].spellID)
+    else
+        lib.callbacks:Fire("PLAYER_TARGET_CHANGED","target",nil,nil)
+    end
+end
+
+local bit_band = _G.bit.band
+local COMBATLOG_OBJECT_TYPE_PLAYER = _G.COMBATLOG_OBJECT_TYPE_PLAYER
+
+function logScanner:COMBAT_LOG_EVENT_UNFILTERED()
+    local _, eventType, _, srcGUID, _, _, _, dstGUID,  _, dstFlags, _, spellID, spellName, _, _, _, _, resisted, blocked, absorbed = CombatLogGetCurrentEventInfo()
+
+    local currTime = GetTime();
+
+    if eventType == "SPELL_CAST_START" then
+        local _, _, icon, castTime = GetSpellInfo(spellID)
+        if not castTime or castTime == 0 then return end
+        local rank = GetSpellSubtext(spellID) -- async so won't work on first try but thats okay
+
+        --Reduce cast time for certain spells
+        -- local reducedTime = castTimeTalentDecreases[spellName]
+        -- if reducedTime then
+        --     castTime = castTime - (reducedTime * 1000)
+        -- end
+        
+        local castID = ""..srcGUID.."_"..spellName..""..currTime; -- Fake a cast GUID
+
+        lib.spellCache[srcGUID] = {
+            castID = castID,
+            spellName = spellName,
+            rank = rank,
+            spellIcon = icon,
+            startTime = currTime,
+            endTime = currTime + castTime,
+            maxValue = castTime,
+            isChanneled = false,    
+            spellID = spellID,
+        }
+
+        if srcGUID == UnitGUID("target") then 
+            lib.callbacks:Fire("UNIT_SPELLCAST_START","target",castID,spellID)
+        end
+
+    elseif eventType == "SPELL_CAST_SUCCESS" then
+        -- Channeled spells are started on SPELL_CAST_SUCCESS instead of stopped
+        -- Also there's no castTime returned from GetSpellInfo for channeled spells so we need to get it from our own list
+        local castTime = channeledSpells[spellName]
+        if castTime then
+            if currTime + castTime > GetTime() then
+                lib.spellCache[srcGUID] = {
+                    castID = 0,
+                    spellName = spellName,
+                    rank = rank,
+                    spellIcon = icon,
+                    startTime = currTime,
+                    endTime = currTime + castTime,
+                    maxValue = castTime,
+                    isChanneled = true,    
+                    spellID = spellID,
+                }
+
+                if srcGUID == UnitGUID("target") then 
+                    lib.callbacks:Fire("UNIT_SPELLCAST_CHANNEL_START","target",0,spellID)
+                end
+            else
+                -- assume the channeled spell is ending
+                if lib.spellCache[srcGUID] then
+                    if srcGUID == UnitGUID("target") then 
+                        lib.callbacks:Fire("UNIT_SPELLCAST_CHANNEL_STOP","target",0,spellID)
+                    end
+                    lib.spellCache[srcGUID] = nil
+                end
+            end
+        end
+        lib.callbacks:Fire("UNIT_SPELLCAST_STOP","target",castID,spellID)
+        lib.spellCache[srcGUID] = nil
+    elseif eventType == "SPELL_CAST_FAILED" then
+        if lib.spellCache[srcGUID] then
+            if srcGUID == UnitGUID("target") then 
+                lib.callbacks:Fire("UNIT_SPELLCAST_FAILED","target",lib.spellCache[srcGUID].castID,lib.spellCache[srcGUID].spellID)
+            end
+            lib.spellCache[srcGUID] = nil
+        end
+    elseif eventType == "SPELL_AURA_APPLIED" then
+        if castTimeDecreases[spellID] then
+            -- Aura that slows casting speed was applied
+            self:CastPushback(dstGUID, namespace.castTimeDecreases[spellID])
+            if lib.spellCache[dstGUID] and dstGUID == UnitGUID("target") then
+                lib.callbacks:Fire("UNIT_SPELLCAST_DELAYED","target",castID,spellID)
+            end 
+        elseif crowdControls[spellName] then
+            if lib.spellCache[dstGUID] then
+                if dstGUID == UnitGUID("target") then 
+                    lib.callbacks:Fire("UNIT_SPELLCAST_STOP","target",castID,spellID)
+                end
+                lib.spellCache[dstGUID] = nil
+            end
+        end
+    elseif eventType == "SPELL_AURA_REMOVED" then
+        -- Channeled spells has no SPELL_CAST_* event for channel stop,
+        -- so check if aura is gone instead since most (all?) channels has an aura effect
+        if channeledSpells[spellName] then
+            if lib.spellCache[srcGUID] then
+                if srcGUID == UnitGUID("target") then 
+                    lib.callbacks:Fire("UNIT_SPELLCAST_STOP","target",castID,spellID)
+                end
+                lib.spellCache[srcGUID] = nil
+            end
+        elseif castTimeDecreases[spellID] then
+             -- Aura that slows casting speed was removed
+            if lib.spellCache[dstGUID] then
+                self:CastPushback(dstGUID, castTimeDecreases[spellID], true)
+                if dstGUID == UnitGUID("target") then
+                    lib.callbacks:Fire("UNIT_SPELLCAST_DELAYED","target",castID,spellID)
+                end    
+            end
+        end
+        elseif eventType == "PARTY_KILL" or eventType == "UNIT_DIED" or eventType == "SPELL_INTERRUPT" then
+            if lib.spellCache[dstGUID] then
+                if dstGUID == UnitGUID("target") then 
+                    lib.callbacks:Fire("UNIT_SPELLCAST_STOP","target",castID,spellID)
+                end
+                lib.spellCache[dstGUID] = nil
+            end
+    elseif eventType == "SWING_DAMAGE" or eventType == "ENVIRONMENTAL_DAMAGE" or eventType == "RANGE_DAMAGE" or eventType == "SPELL_DAMAGE" then
+        if resisted or blocked or absorbed then return end
+        if bit_band(dstFlags, COMBATLOG_OBJECT_TYPE_PLAYER) > 0 then -- is player
+            if lib.spellCache[dstGUID] then
+                self:CastPushback(dstGUID)
+                if dstGUID == UnitGUID("target") then
+                    lib.callbacks:Fire("UNIT_SPELLCAST_DELAYED","target",castID,spellID)
+                end    
+            end
+        end
+    end
+end
